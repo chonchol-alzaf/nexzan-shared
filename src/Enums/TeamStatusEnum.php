843ce73cb7a8cbd58689b32ea5751233e2e31f08
Nafis Chonchol <145265617+chonchol-alzaf@users.noreply.github.com>
2025-11-09 11:17:12 +0600

    Update TeamStatusEnum.php

--- src/Enums/TeamStatusEnum.php
+++ src/Enums/TeamStatusEnum.php
@@ -7,7 +7,7 @@ use BenSampo\Enum\Enum;
 final class TeamStatusEnum extends Enum
 {
     const ACTIVE       = 1;
-    const TRAIL        = 2;
+    const TRIAL        = 2;
     const INACTIVE     = 3;
     const SUSPEND      = 4;
     const TRIAL_ENDED  = 5;
