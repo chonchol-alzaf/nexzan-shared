42d457748fb54e90f0deb06e6d66bee4e7124815
Nafis Chonchol <145265617+chonchol-alzaf@users.noreply.github.com>
2026-06-15 16:35:30 +0600

    Update TeamStatusEnum.php

--- src/Enums/TeamStatusEnum.php
+++ src/Enums/TeamStatusEnum.php
@@ -2,13 +2,30 @@
 
 namespace Nexzan\Shared\Enums;
 
-use BenSampo\Enum\Enum;
-
-final class TeamStatusEnum extends Enum
+enum TeamStatusEnum: int
 {
-    const ACTIVE       = 1;
-    const TRIAL        = 2;
-    const INACTIVE     = 3;
-    const SUSPEND      = 4;
-    const TRIAL_ENDED  = 5;
+    case ACTIVE = 1;
+    case TRIAL = 2;
+    case INACTIVE = 3;
+    case SUSPEND = 4;
+    case TRIAL_ENDED = 5;
+
+    public static function getKey(self|int|string|null $value): ?string
+    {
+        if ($value instanceof self) {
+            return $value->name;
+        }
+
+        return self::tryFrom((int) $value)?->name;
+    }
+
+    public static function values(): array
+    {
+        return array_column(self::cases(), 'value');
+    }
+
+    public static function getValues(): array
+    {
+        return self::values();
+    }
 }
